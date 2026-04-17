e711c6df1da9d52c083b9de75419291f1f91f846
Sanya Gupta <guptasanya366@gmail.com>
2026-04-17 22:59:11 +0530

    add firebase id column to users

--- backend/db/models.py
+++ backend/db/models.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id            = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    firebase_uid = Column(String,unique=True, nullable=True)
     username      = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
     created_at    = Column(DateTime, default=datetime.utcnow)
