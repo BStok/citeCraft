bb6ebebb9df6ac44c496f41956fddce1ac2fd46a
Sanya Gupta <guptasanya366@gmail.com>
2026-04-18 11:10:52 +0530

    initialize admin sdk

--- backend/auth/firebase_auth.py
+++ backend/auth/firebase_auth.py
@@ -1,9 +1,13 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, auth
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase.json")
+    cred_dict = json.loads(os.environ["FIREBASE_SERVICE_ACCOUNT"])
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
+
 
 def verify_firebase_token(token: str):
     try:
