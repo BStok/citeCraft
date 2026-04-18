5e5a8750a368fa91375617dbd8362447a4bb1002
Sanya Gupta <guptasanya366@gmail.com>
2026-04-18 12:28:37 +0530

    updated firebase.ts

--- frontend/ver1/client/src/lib/firebase.ts
+++ frontend/ver1/client/src/lib/firebase.ts
@@ -2,9 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyC7jbWFLH4r8ipoGTsNoYuiKecWHRJxwLU",
+  authDomain: "et-al-asst.firebaseapp.com",
+  projectId: "et-al-asst",
+  storageBucket: "et-al-asst.firebasestorage.app",
+  messagingSenderId: "80260706822",
+  appId: "1:80260706822:web:92a69155cfe871f8858987",
+  measurementId: "G-9MJ486Z8VF"
 };
 
 const app = initializeApp(firebaseConfig);
